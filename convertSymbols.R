a4b327ad087676a572623a0b585ec603c5ad88bd
Charlene Miciano <charlenemiciano@Charlenes-MacBook-Air.local>
2019-10-10 14:42:24 -0700

    convert input file of genes to gene symbol given file name, genome, and ID type

---
+++ convertSymbols.R
@@ -0,0 +1,78 @@
+#!/usr/bin/env Rscript
+
+#### Takes in a gene list as a text file, a genome (either mm10 or hg19),
+## and ID type (geneID, Unigene, RefSeq, Ensembl, name) and converts
+## them to gene symbol
+args<-commandArgs(trailingOnly=T)
+
+infile_name = args[1]
+cat("Reading in gene list:", infile_name, "\n")
+inputgenesdata <- read.delim(infile_name, header = F, stringsAsFactors = F)
+inputgenesdata <-read.delim("inputgenes.txt", stringsAsFactors = F, header = F)
+
+## Example file 
+#GeneID
+#inputgenesdata <- read.delim("~/Documents/Salk/TFMatrix/31geneidmm10.txt",
+#                             stringsAsFactors = F, header = F)
+
+#Unigene
+#inputgenesdata <- read.delim("~/Documents/Salk/TFMatrix/31geneunimm10.txt",
+#                             stringsAsFactors = F, header = F)
+
+#RefSeq
+
+#inputgenesdata <- read.delim("~/Documents/Salk/TFMatrix/31generefmm10.txt",
+#                            stringsAsFactors = F, header = F)
+#Ensembl
+#inputgenesdata <- read.delim("~/Documents/Salk/TFMatrix/31geneensmm10.txt",
+#                            stringsAsFactors = F, header = F)
+
+
+names(inputgenesdata) <- "gene_symbol"
+
+ref = args[2]
+if (ref == "mm10") {
+  cat("Using mm10 genome\n")
+  generef <- read.delim("~/mm10_GeneIDsymbol.tsv", 
+                        header=TRUE, stringsAsFactors = F)
+  
+} else if(ref == "hg19") {
+  cat("Using hg19 genome\n")
+  generef <- read.delim("~/hg19_GeneIDsymbol.tsv", 
+                        header=TRUE, stringsAsFactors = F)
+  
+} else {
+  print("Not a valid genome")
+}
+
+
+idtype = args[3]
+
+
+if(idtype == "geneid") {
+  cat("Converting input gene ID to gene symbol...\n")
+  sub <- subset(generef,  generef$GeneID %in% inputgenesdata$gene_symbol)
+  
+}else if(idtype == "unigene") {
+  cat("Converting input Unigene IDs to gene symbol...\n")
+  sub <- subset(generef,  generef$Unigene %in% inputgenesdata$gene_symbol)
+  
+}else if(idtype == "refseq") {
+  cat("Converting input RefSeq IDs to gene symbol...\n")
+  sub <- subset(generef,  generef$RefSeq %in% inputgenesdata$gene_symbol)
+
+  } else if(idtype == "ensembl") {
+  cat("Converting input Ensembl IDs to gene symbol...\n")
+  sub <- subset(generef,  generef$Ensembl %in% inputgenesdata$gene_symbol)
+
+  } else if (idtype == "name") {
+  cat("Taking in name IDs...\n")
+  sub <- subset(generef,  generef$name %in% inputgenesdata$gene_symbol)
+  }
+
+#Converts all forms to ENS
+inputgenesdata <- as.character(sub$name)
+inputgenesdata <- data.frame(inputgenesdata)
+names(inputgenesdata) <- "name"
+write.table(inputgenesdata, "outputgenes.txt", row.names = F, col.names = F, quote = F)
+#cat(nrows(inputgenesdata))

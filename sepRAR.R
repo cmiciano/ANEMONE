a15faf5049ba05908528263f80973a97868eeddb
Charlene Miciano <charlenemiciano@Charlenes-MacBook-Air.local>
2019-10-15 11:43:17 -0700

    separate RAR:RXR files before annotation and column generation

---
+++ sepRAR.R
@@ -0,0 +1,18 @@
+### Script to separate RAR:RXR motif file into separate files
+files <- list.files(pattern = "RAR:RXR")
+filesm <- grep(".m", files, value = T)
+filesm
+origmotif <- read.delim("filesm", header = F, stringsAsFactors = F)
+colnames(origmotif)[7]<- "Focus.Ratio.Region.Size"
+RARsplit <- split(origmotif, nchar(origmotif$Focus.Ratio.Region.Size))
+
+
+RAR12 <- RARsplit[["12"]]
+write.table(RAR12, 
+            paste("RAR:RXR(NR),DR5|ES-RAR-ChIP-Seq(GSE56893)","12.m", sep = "-"),
+            row.names = F , col.names = F, , sep = "\t")
+
+RAR18 <- RARsplit[["18"]]
+write.table(RAR18, 
+            paste("RAR:RXR(NR),DR5|ES-RAR-ChIP-Seq(GSE56893)","18.m", sep = "-"),
+            row.names = F , col.names = F, , sep = "\t")

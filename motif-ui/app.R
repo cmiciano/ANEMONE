d85f2e23b8762142c24b4838208faba6d3f3541f
Charlene Miciano <charlenemiciano@Charlenes-MacBook-Air.local>
2019-10-29 12:04:13 -0700

    generated heatmap, created pca plot download

--- motif-ui/app.R
+++ motif-ui/app.R
@@ -3,8 +3,10 @@
 # the 'Run App' button above.
 source('convertSymbols.R')
 source('subsetGenes.R')
+source('genHeatmap.R')
 library(shiny)
 library(ggplot2)
+library(gplots)
 
 #source("census-app/helpers.R")
 #counties <- readRDS("census-app/data/counties.rds")
@@ -76,8 +78,11 @@ ui <- fluidPage(
       
       tabsetPanel(type = "tabs",
                   tabPanel("Input Genes", tableOutput("contents"), tableOutput("changed")),
-                  tabPanel("PCA", plotOutput("plot")),
-                  tabPanel("Heatmap", verbatimTextOutput("summary")),
+                  tabPanel("PCA", plotOutput("plot"),
+                            downloadButton("save", "Download")),
+                  #tabPanel("Heatmap", tableOutput("heatmap")),
+                  tabPanel("Heatmap", plotOutput("heatmap"),
+                           downloadButton("downloadData", "Download")),
                   tabPanel("Network Graph", tableOutput("table"))
       )
     )
@@ -129,11 +134,13 @@ server <- function(input, output) {
     paste("You chose", input$genome)
   })
   
-  output$plot <- renderPlot({
+  
+  
+  individualGraph <- reactive({
     if(is.null(input$file1))  
       return(NULL) 
     #newgenes <- convertGenes(input$file1$datapath, input$genome, input$idtype)
-   # pcaDF <- generatePCA(input$file1$datapath)
+    # pcaDF <- generatePCA(input$file1$datapath)
     pcaDF <- generatePCA(newgenes(),input$genome)
     
     #ggplot(pcaDF)
@@ -145,19 +152,99 @@ server <- function(input, output) {
     ymax <- round(max(pcaDF$PC2))+1
     ymin <- round(min(pcaDF$PC2))-1
     ggplot(pcaDF,aes(PC1 ,PC2)) +
-       ggtitle("Genes Grouped by Common Motifs") +
-       xlab(paste("PC1",round(pc1var * 100, 0), "%"))+
-       ylab(paste("PC2",round(pc2var * 100, 0), "%")) +
-       xlim(xmin, xmax) + 
-       ylim(ymin,ymax) +
-       #geom_text(label= groupnum, size = 3, nudge_x = 1 ,nudge_y = 2) +
-       #scale_fill_discrete(name = "Grouping") + 
-       geom_point(size = 3) +
-       #geom_step() +
-       #scale_color_manual(values = c("#7FC97F","#F0027F","#386CB0"))
-       theme_classic() 
+      ggtitle("Genes Grouped by Common Motifs") +
+      xlab(paste("PC1",round(pc1var * 100, 0), "%"))+
+      ylab(paste("PC2",round(pc2var * 100, 0), "%")) +
+      xlim(xmin, xmax) + 
+      ylim(ymin,ymax) +
+      #geom_text(label= groupnum, size = 3, nudge_x = 1 ,nudge_y = 2) +
+      #scale_fill_discrete(name = "Grouping") + 
+      geom_point(size = 3) +
+      #geom_step() +
+      #scale_color_manual(values = c("#7FC97F","#F0027F","#386CB0"))
+      theme_classic() 
     # dev.off()
   })
+  
+  
+  output$plot <- renderPlot({
+    req(individualGraph())
+    individualGraph()
+   #  if(is.null(input$file1))  
+   #    return(NULL) 
+   #  #newgenes <- convertGenes(input$file1$datapath, input$genome, input$idtype)
+   # # pcaDF <- generatePCA(input$file1$datapath)
+   #  pcaDF <- generatePCA(newgenes(),input$genome)
+   #  
+   #  #ggplot(pcaDF)
+   #  groupnum <- rownames(pcaDF)
+   #  ## new plot with 150
+   #  xmax <- round(max(pcaDF$PC1))+1
+   #  xmin <- round(min(pcaDF$PC1))-1
+   #  
+   #  ymax <- round(max(pcaDF$PC2))+1
+   #  ymin <- round(min(pcaDF$PC2))-1
+   #  ggplot(pcaDF,aes(PC1 ,PC2)) +
+   #     ggtitle("Genes Grouped by Common Motifs") +
+   #     xlab(paste("PC1",round(pc1var * 100, 0), "%"))+
+   #     ylab(paste("PC2",round(pc2var * 100, 0), "%")) +
+   #     xlim(xmin, xmax) + 
+   #     ylim(ymin,ymax) +
+   #     #geom_text(label= groupnum, size = 3, nudge_x = 1 ,nudge_y = 2) +
+   #     #scale_fill_discrete(name = "Grouping") + 
+   #     geom_point(size = 3) +
+   #     #geom_step() +
+   #     #scale_color_manual(values = c("#7FC97F","#F0027F","#386CB0"))
+   #     theme_classic() 
+   #  # dev.off()
+  })
+  
+  output$save <- downloadHandler(
+    filename = "save.png" ,
+    content = function(file) {
+      #ggsave(p(), filename = file)
+      req(individualGraph())
+      ggsave(file, plot = individualGraph(), device = 'png')
+      dev.off()
+    })
+  # output$heatmap <- renderTable({
+  #   if(is.null(input$file1))  
+  #     return(NULL) 
+  #   map <- genHeatmap(newgenes(),input$genome) #should return table
+  #   return(map)
+  # })
+  
+  output$heatmap <- renderPlot({
+    if(is.null(input$file1))  
+      return(NULL) 
+    mapmat <<- genHeatmap(newgenes(),input$genome) #should return table
+    
+    heatmap.2(as.matrix(mapmat), Rowv = T, Colv = T, col = heat.colors, 
+              trace = "none", labRow = rownames(targetmatnmum),
+              #lhei = c(0.5,5),     
+              #lhei = c(0.5,1),     
+              #lwid = c(0.5,0.5),
+              cexRow=0.15,
+              cexCol=0.15,
+              hclustfun=function(x) hclust(x, method="ward.D"))
+             # invisible(dev.off())
+              cat("Heatmap finished!\n")
+              
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      #"out.txt"
+      gsub(".*\\.","outputgenes.",input$file1)
+      #paste(input$file1, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(newgenes(), file, row.names = FALSE, col.names = F, quote = F)
+    }
+  )
+
+  
+  
   
 }
 # Run the app ----
---
+++ motif-ui/genHeatmap.R
@@ -0,0 +1,98 @@
+genHeatmap <- function(infile_name,genome) {
+  
+  #infile_name=args[1]
+  cat("Input file name: ",nrow(infile_name), "\n")
+  #inputgenes <-read.delim(infile_name, stringsAsFactors = F, header = F)
+  
+  #inputgenes <-read.delim("inputgenes.txt", stringsAsFactors = F, header = F)
+  #inputvec <- readLines("../inputgenes.txt")
+  #inputgenes <- read.delim("~/Documents/Salk/TFMatrix/33geneuni.txt", header = F)
+  #inputgenes <-read.delim("../inputgenes.txt", stringsAsFactors = F, header = F) #works
+  inputgenes <- infile_name
+  cat("inputgenes\n")
+  print(class(inputgenes))
+  print(dim(inputgenes))
+  
+  print(head(inputgenes[1:10,1]))
+  #inputvec <- readLines(infile_name)
+  
+  #inputvec <- as.vector(inputgenes)
+  colnames(inputgenes) <- "name"
+  
+  #inputvec <- inputvec[1:10]
+  if (genome == "hg19") {
+    allTFcounts <- read.delim("data/allTFcounts.txt", stringsAsFactors=FALSE)
+    
+  } else if (genome == "mm10") {
+    allTFcounts <- read.delim("data/allTFcountsmm10.txt", stringsAsFactors=FALSE)
+    
+  }
+  
+  
+  ### Subset expression matrix by the representative motifs
+  repmotifs <- read.delim("data/163repmotifssummary.txt", stringsAsFactors=FALSE)
+  
+  
+  
+  #View(colnames(allTFcounts))
+  colallTF <- colnames(allTFcounts)
+  #View(repmotifs$short_rep_motif)
+  shortTF <- repmotifs$short_rep_motif
+  
+  allTFsub <- subset(allTFcounts, colallTF %in% shortTF)
+  allTFsub <- allTFcounts[,colallTF %in% shortTF]
+  ID <- allTFcounts[,1]
+  allTFsub <- cbind(ID, allTFsub) #allTFsub represents the 20k ish genes x 163 rep motifs
+  
+  
+  ### Subset allTFcounts.txt into the representative TFs
+  
+  #genelist <- c("A1CF", "AACS", "AADACL4", "AADAT")
+  
+  #Sample
+  set.seed(1)
+  #inputgenes <- sample(allTFcounts$ID, 1000, replace = F)
+  #write.table(inputgenes, "inputgenes.txt", quote = F, row.names = F, col.names = F)
+  cat("Subsetting table based on genes of interest...\n")
+  targetgenes <- subset(allTFsub, allTFsub$ID %in% inputgenes$name)
+  cat("target genes\n")
+  cat(nrow(targetgenes))
+  #targetgenes <- subset(allTFcounts, allTFcounts$ID %in% inputvec)
+  
+  motifnm <- colnames(targetgenes)
+  genenm <- targetgenes$ID
+  
+  targetmatnmum <- targetgenes[,2:ncol(targetgenes)]
+  cat(head(targetmatnmum[1:10,1]))
+  #targetmatnmum <- expint[,2:ncol(expint)]
+  
+  rownames(targetmatnmum) <- genenm
+  targetmatnmum <- log(targetmatnmum+5) #genes by motif
+  
+  cat(head(targetmatnmum[1:10,1]))
+  
+  # ## Generate a heatmap of the counts of each motif occurrence in 
+  # ## the promoter region of each gene
+  # cat("Generating heatmap...\n")
+  # #pdf("genemotifctint.pdf", 8, 15)
+  # pdf("genemotifctint.pdf", 4, 4)
+  # #par(mar=c(10,2,2,3), cex=1.0)
+  # par(mar=c(1,1,1,1), cex=1.0)
+  # 
+  # heatmap.2(as.matrix(targetmatnmum), Rowv = T, Colv = T, col = heat.colors, 
+  #           trace = "none", labRow = rownames(targetmatnmum),
+  #           #lhei = c(0.5,5),     
+  #           lhei = c(0.5,1),     
+  #           #lwid = c(0.5,0.5),
+  #           cexRow=0.15,
+  #           cexCol=0.15)
+  #           #hclustfun=function(x) hclust(x, method="ward.D"))
+  # invisible(dev.off())
+  # cat("Heatmap finished!\n")
+  
+  
+  
+  return(targetmatnmum) ##Return PCA plot to plot in shiny app
+  
+}
+
